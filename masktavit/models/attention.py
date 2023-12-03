5e38349e4e1234474e098fe3490cf542a6378e85
Ruochen Zhang <111889976+zrc0123@users.noreply.github.com>
2023-12-03 01:31:08 -0500

    Update attention.py
    Support frame conditioned attention

--- masktavit/models/attention.py
+++ masktavit/models/attention.py
@@ -521,6 +521,30 @@ def scaled_dot_product_attention(q, k, v, mask=None, attn_dropout=0., training=T
     if mask is not None:
         attn = attn.masked_fill(mask == 0, float('-inf'))
     if distance_mask is not None:
+        def scaled_dot_product_attention(q, k, v, mask=None, attn_dropout=0., training=True, distance_mask=None):
+    # Performs scaled dot-product attention over the second to last dimension dn
+
+    # (b, n_head, d1, ..., dn, d)
+    attn = torch.matmul(q, k.transpose(-1, -2))
+    attn = attn / np.sqrt(q.shape[-1])
+    if mask is not None:
+        attn = attn.masked_fill(mask == 0, float('-inf'))
+    if distance_mask is not None:
+        if distance_mask.shape == attn.shape[-2:]:
+            attn = torch.mul(distance_mask.type_as(attn),attn)
+        else:
+            t = int(distance_mask.shape[0]/ attn.shape[-1])
+            hw =  attn.shape[-1]
+            distance_mask=distance_mask[0:hw][0:hw]
+            attn = torch.mul(distance_mask.type_as(attn),attn)
+            
+    attn_float = F.softmax(attn, dim=-1)
+    attn = attn_float.type_as(attn) # b x n_head x d1 x ... x dn x d
+    attn = F.dropout(attn, p=attn_dropout, training=training)
+
+    a = torch.matmul(attn, v) # b x n_head x d1 x ... x dn x d
+
+    return a
         
         attn = torch.mul(distance_mask.type_as(attn),attn)
     attn_float = F.softmax(attn, dim=-1)
